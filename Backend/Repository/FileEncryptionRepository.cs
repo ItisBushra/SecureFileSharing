abea79918cb56d209c3658370ad7a4383a244ce8
ThePagliacci <boshrakhalid6@gmail.com>
2025-07-24 14:28:04 +0300

    Repository Functionalities Added

---
+++ Backend/Repository/FileEncryptionRepository.cs
@@ -0,0 +1,55 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Backend.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repository
+{
+    public class FileEncryptionRepository : IFileEncyptionRepository
+    {
+        private DBContext _dBContext;
+        public FileEncryptionRepository(DBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+        public Task<bool> CheckEncryptedFileExpirationDateAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<FileEncryption> CreateEncryptedFileAsync(FileEncryption fileEncryption)
+        {
+            await _dBContext.FileEncryption.AddAsync(fileEncryption);
+            await _dBContext.SaveChangesAsync();
+            return fileEncryption;
+        }
+
+        public async Task<bool> RemoveEncryptedFileAsync(Guid id)
+        {
+            var fileToBeRemoved = await _dBContext.FileEncryption.FindAsync(id);
+            if (fileToBeRemoved != null)
+            {
+                _dBContext.Remove(fileToBeRemoved);
+                await _dBContext.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> UpdateDownloadCountAsync(Guid id)
+        {
+            var fileToBeUpdated = await _dBContext.FileEncryption.FindAsync(id);
+            if (fileToBeUpdated != null)
+            {
+                fileToBeUpdated.DownloadCount += 1;
+                await _dBContext.SaveChangesAsync();
+                return true;    
+            }
+            return false;
+        }
+    }
+}
---
+++ Backend/Repository/IRepository/IFileEncyptionRepository.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repository.IRepository
+{
+    public interface IFileEncyptionRepository
+    {
+        Task<FileEncryption> CreateEncryptedFileAsync(FileEncryption fileEncryption); 
+        Task<bool> RemoveEncryptedFileAsync(Guid id);
+        Task<bool> CheckEncryptedFileExpirationDateAsync(Guid id);
+        Task<bool> UpdateDownloadCountAsync(Guid id);
+    }
+}
